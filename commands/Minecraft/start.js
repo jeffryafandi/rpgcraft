88113f7e9e4ebf482c6f726f3db792cccd73fdd8
Glitch (hello-express) <none>
2020-09-01 11:15:17 +0000

    👞💎 Checkpoint ./commands/Minecraft/start.js:21789943/697 ./watch.json:21789943/9

--- commands/Minecraft/start.js
+++ commands/Minecraft/start.js
@@ -3,9 +3,10 @@ const Discord = require('discord.js')
 module.exports.run = async (client,msg,args) => {
  
   //Var
-  var mCancel = `You have canceled the command`
+  var mCancel = `You canceled the command`
   var tErr = `Time has run out, please repeat the command again!`
   
+  const tag = `**${msg.author.tag}**`
   
   
   //Filter
@@ -13,17 +14,14 @@ module.exports.run = async (client,msg,args) => {
    m.author.id === msg.author.id 
   }
   
-  const wkt = {
-    max: 1,
-    time: 20000,
-    errors:['time']
-  }
+  
         
   
   
   //
   
   /* Await Nickname */
+  try {
   const embee = new Discord.MessageEmbed()
   .setColor(client.config.COLOR.RANDOM)
   .setAuthor(`${client.user.username} Adventure | Nickname`, client.user.displayAvatarURL())
@@ -32,16 +30,21 @@ module.exports.run = async (client,msg,args) => {
   .setFooter(`Adventure Setup 1 / 3 | Type cancel to cancel the action!`)
 const emb1 = await msg.channel.send(embee)
 
-await msg.channel.awaitMessages(filter, wkt).then((res) => {
-  let kj = res.firts().content.toLowerCase()
+await msg.channel.awaitMessages(filter, { max: 1, time: 20000, errors:['time'] }).then(res => {
+  let kj = res.first().content.toLowerCase()
+  console.log(res)
   
   if(kj === "cancel") {
     emb1.delete()
-    return msg.channel.send(`${tag}, Your gender has been stored in the database! (\`Female\`)`).then(m => m.delete({timeout:5000}))
+    return msg.channel.send(mCancel).then(k => k.delete({timeout:10000}))
   }
   
+  msg.channel.send(`${tag}, Your nickname has been stored in the database! (\`${kj}\`)`).then(l => l.delete({timeout:10000}))
+  
 })
-
+  } catch (err) {
+    console.log(err)
+  }
   
   
   /* End Nickname */
