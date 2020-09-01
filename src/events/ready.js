320bb58308771dc5460395be2b81c75822478d9f
Glitch (hello-express) <none>
2020-09-01 01:52:16 +0000

    🚛💵 Checkpoint ./src/events/ready.js:21789943/247 ./watch.json:21789943/1

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,6 @@
 module.exports = client => {
     console.log(`Logged in as ${client.user.tag}`);
     
-        client.user.setPresence({
-            status: 'dnd',
-            activity: {
-                name: `Beta Version`,
-                type: 'WATCHING',
-                url: '-'
-            }
-        });
+        client.user.setActivity(`Beta Version`, { type: "PLAYING"})
+        
 }
