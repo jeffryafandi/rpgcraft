a54cb6601eb66cb6aa91e1229c008d2878d65297
Glitch (hello-express) <none>
2020-09-01 07:35:53 +0000

    ⚓️🖼 Checkpoint ./commands/Minecraft/start.js:17473877/20:21789943/308 ./watch.json:21789943/1

--- commands/Minecraft/start.js
+++ commands/Minecraft/start.js
@@ -31,6 +31,9 @@ msg.channel.send(`**${msg.author.tag}**, Your nickname has been stored in the da
   const lkk = await emb1.edit(jk)
   
   await msg.channel.awaitMessages(filter, { max: 1, time: 20000, errors:['time']}).then(res => {
+    
+    msg.delete()
+    
     if(res.first().content.toLowerCase() === "cancel") {
       lkk.delete()
       return msg.channel.send(`**${msg.author.tag}**, You have canceled the setup, you can start it any time!`).then(i => i.delete({timeout:10000}))
@@ -38,9 +41,14 @@ msg.channel.send(`**${msg.author.tag}**, Your nickname has been stored in the da
       msg.channel.send(`**${msg.author.tag}**, Your gender has been stored in the database! (\`male\`)`).then(w => w.delete({timeout:5000}))
     } else if(res.first().content.toLowerCase() === "female") {
       msg.channel.send(`**${msg.author.tag}**, Your gender has been stored in the database! (\`female\`)`).then(m => m.delete({timeout:5000}))
-    } hola
+    } else if(res.first().content.toLowerCase()) {
+      lkk.delete()
+      msg.channel.send(`**${msg.author.tag}**, Your gender is not valid!`).then(w => w.delete({timeout:5000}))
+      return;
+    }
+            
   }) 
-    
+
   }
                                                                    
 
