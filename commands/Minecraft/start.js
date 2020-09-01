c0407accb9d8b0cc03d3e89eca7c0f14cac9bbad
Glitch (hello-express) <none>
2020-09-01 11:38:19 +0000

    🚌🌷 Checkpoint ./commands/Minecraft/start.js:21789943/2524 ./watch.json:21789943/1

--- commands/Minecraft/start.js
+++ commands/Minecraft/start.js
@@ -15,6 +15,18 @@ module.exports.run = async (client,msg,args) => {
     if(m.author.id === msg.author.id) return true
   }
   
+  
+  const filter2 = m => {
+   if(m.author.bot) return
+    if(m.author.id === msg.author.id && m.content === "male" && m.content.toLowerCase() === "female" && m.content.toLowerCase() === "cancel") return true
+    else {
+      msg.delete()
+     msg.channel.send(`${tag}, Gender is not valid!`)
+      return false
+    }
+    
+  }
+  
   const wkt = {
     max: 1,
     time: 30000,
@@ -37,7 +49,7 @@ module.exports.run = async (client,msg,args) => {
   .setFooter(`Adventure Setup 1 / 3 | Type cancel to cancel the action!`)
 const emb1 = await msg.channel.send(embee)
 
-await msg.channel.awaitMessages(filter, { max: 1, time: 20000, errors:['time'] }).then(res => {
+await msg.channel.awaitMessages(filter, wkt).then(res => {
   let kj = res.first().content.toLowerCase()
   console.log(res)
   
@@ -47,12 +59,52 @@ await msg.channel.awaitMessages(filter, { max: 1, time: 20000, errors:['time'] }
   }
   data.push(kj)
   msg.channel.send(`${tag}, Your nickname has been stored in the database! (\`${kj}\`)`).then(l => l.delete({timeout:5000}))
-})
+}).catch(err => {
+    console.log(err)
+    return msg.reply(`Time has run out, please repeat the command again!`).then(e => e.delete({timeout:10000}))
+  })
+  
   /* End Nickname */
-
+  
+  
   /* Await Gender */
   
-  /*Emd
+const jk = new Discord.MessageEmbed()
+  .setColor(client.config.COLOR.RANDOM)
+  .setAuthor(`${client.user.username} Adventure | Gender`, client.user.avatarURL())
+  . setDescription (`**${msg.author.tag}** Welcome to adventure ${client.user.username}. To get started, please use the instructions provided below`)
+  .addField(`Input`, `Enter gender! \`Male or Female\``)
+  .setFooter(`Adventure Setup 2 / 3 | Type cancel to cancel the action`)
+  emb1.delete()
+  const lkk = await msg.channel.send(jk)
+  
+  await msg.channel.awaitMessages(filter2, wkt).then(res => {
+    let rt = res.first().content.toLowerCase()
+    
+    if(rt === "cancel") {
+      lkk.delete()
+      return msg.channel.send(`${tag}, You have canceled the setup, you can start it any time!`).then(i => i.delete({timeout:10000}))
+    } else if(rt === "male") {
+      data.push("male")
+    return  msg.channel.send(`${tag}, Your gender has been stored in the database! (\`Male\`)`).then(w => w.delete({timeout:5000}))
+    } else if(rt === "female") {
+      data.push("female")
+     return msg.channel.send(`${tag}, Your gender has been stored in the database! (\`Female\`)`).then(m => m.delete({timeout:5000}))
+    }
+    
+    
+    
+  }).catch(err => {
+    console.log(err)
+    return msg.reply(`Time has run out, please repeat the command again!`).then(e => e.delete({timeout:10000}))
+  })
+  
+  
+  
+  
+  
+  
+  /*End Gender*/
 }
 
 
