37c72fde360a31d2fb62c2bd19ae474167889b81
Glitch (hello-express) <none>
2020-09-02 02:09:44 +0000

    🎪🛋 Checkpoint ./commands/Minecraft/help.js:17473877/438 ./watch.json:17473877/6 ./commands/Minecraft/mdl.json:17473877/18

--- commands/Minecraft/help.js
+++ commands/Minecraft/help.js
@@ -10,10 +10,18 @@ module.exports.run = async (client,msg,args) => {
   .setAuthor(`Help Commands ${client.user.username}`, client.user.avatarURL())
   .setDescription(`Support Server: https://discord.gg/ayam`)
   for(const mod of help) {
-   embee.addField(`${mod.name}`, mod.cmds.map(x => `\`${x}\``).join(", "))
+   embee.addField(`${mod.name}`, mod.cmds.filter(()=> mod.hide === false).map(x => `\`${x}\``).join(", "))
   }
   msg.channel.send(embee)
-  
+  if (args[0].toLowerCase() === "other"){
+    const help = client.helps.array()
+    const embed = new MessageEmbed()
+    .setColor(client.config.COLOR.RANDOM)
+    .setAuthor(`Help Commands ${client.user.username}`, client.user.avatarURL())
+    for(const mod of help){
+      embed.addField
+    }
+  }
 }
 
 module.exports.help = {
