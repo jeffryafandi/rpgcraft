65dd69d43fd22ce13b7e4fc44c51075a5144e041
Glitch (hello-express) <none>
2020-09-02 01:46:44 +0000

    📈🦁 Checkpoint ./watch.json:17473877/10:21789943/1 ./commands/Minecraft/skin.js:17473877/246 ./commands/Minecraft/help.js:21789943/515 ./package.json:17473877/60

---
+++ commands/Minecraft/help.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require ('discord.js')
+
+
+module.exports.run = async (client,msg,args) => {
+  
+  const embee = new MessageEmbed()
+  .setColor(client.config.COLOR.RANDOM)
+  .setAuthor(`Help Commands ${client.user.username}`, client.user.avatarURL())
+  .setDescription(`Support Server: https://discord.gg/ayam`)
+  for
+  
+}
+
+module.exports.help = {
+  name: "help",
+  aliases: ['h', 'cmd']
+}
+  
+  
